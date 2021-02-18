3190757e97bcf77c43b0a2862ea041d7c297203e
Azhar Zaman <68331974+TheAzharZaman@users.noreply.github.com>
2021-02-18 14:15:03 +0500

    Minor Updated Being Pushed

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -7,6 +7,14 @@ const Welcome = () => {
       <h1> Welcome </h1>
       <h3>To</h3>
       <h2>Vitual Assitant Application (Beta V0.1) </h2>
+      <div className="copyright__section">
+        <h3>
+          Design & Developed by{" "}
+          <a target="_blank" href="https://azharzaman.com">
+            Azhar Zaman
+          </a>
+        </h3>
+      </div>
     </div>
   );
 };
